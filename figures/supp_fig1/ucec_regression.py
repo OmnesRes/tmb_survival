ada239ed9dfcdda67c009a8bef9210a3baed302f
Jordan Anaya <omnesresnetwork@gmail.com>
2024-03-22 18:28:39 -0400

    working on new figure

--- figures/supp_fig1/ucec_regression.py
+++ figures/supp_fig1/ucec_regression.py
@@ -7,14 +7,14 @@ else:
 import sys
 sys.path.append(str(cwd))
 from matplotlib import pyplot as plt
-from lifelines import KaplanMeierFitter
 import numpy as np
-from lifelines import CoxPHFitter
-import pandas as pd
-from lifelines.statistics import logrank_test
+from tqdm import tqdm
+import concurrent.futures
 from model import utils
+from lifelines import KaplanMeierFitter
+from lifelines import CoxPHFitter
+from lifelines.statistics import logrank_test
 import pickle
-
 
 data = pickle.load(open(cwd / 'files' / 'data.pkl', 'rb'))
 samples = pickle.load(open(cwd / 'files' / 'tcga_public_sample_table.pkl', 'rb'))
@@ -74,3 +74,117 @@ ax.spines['bottom'].set_position(['outward', 5])
 ax.spines['left'].set_bounds(.7, 1)
 ax.legend(frameon=False, title='TMB', ncol=5)
 plt.savefig(cwd / 'figures' / 'supp_fig1' / 'survival_curves.pdf')
+
+
+t = utils.LogTransform(bias=4, min_x=0)
+tmb = t.trf(tmb)
+
+bin = .1
+counts = dict(zip(*np.unique(np.around(tmb, 1), return_counts=True)))
+counts.update({round(i, 1): counts.get(round(i, 1), 0) for i in range(0, int(t.trf(1000) / 10))})
+values = [sum([counts.get((i / 10) + j / 10, 0) for j in range(int(bin * 10))]) for i in range(int(min(counts.keys()) * 10), int(max(counts.keys()) * 10) + 1, int(bin * 10))]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.subplots_adjust(bottom=.108)
+fig.subplots_adjust(top=1)
+fig.subplots_adjust(left=.1)
+fig.subplots_adjust(right=.97)
+ax.bar([i / 10 for i in range(int(min(counts.keys()) * 10), int(max(counts.keys()) * 10) + 1, int(bin * 10))], values, width=bin, alpha=.3)
+ax.set_xticks(t.trf(np.array([0, 2, 5, 10, 20, 40, 100, 200, 500, 1000])))
+ax.set_xticklabels([0, 2, 5, 10, 20, 40, 100, 200, 500, 1000])
+ax.set_yticks(list(range(0, 100, 10)))
+ax.set_yticklabels(list(range(0, 100, 10)))
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_bounds(0, t.trf(1000))
+ax.spines['left'].set_bounds(0, 90)
+ax.set_xlabel('TMB', fontsize=12)
+ax.set_ylabel('Counts', fontsize=12)
+plt.savefig(cwd / 'figures' / 'supp_fig1' / 'log_histogram.pdf')
+
+cph = CoxPHFitter()
+samples['tmb'] = tmb
+cph.fit(samples, duration_col='OS.time', event_col='OS', formula="tmb")
+x_times = np.linspace(0, 365 * 5, 200)
+survival_probs = cph.predict_survival_function(t.trf(np.array([1, 10, 64, 256, 512]))[:, np.newaxis], x_times)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=.98)
+fig.subplots_adjust(right=.99)
+for index, tmb in enumerate([1, 10, 64, 256, 512]):
+    ax.plot(x_times, survival_probs.values[:, index], label=tmb)
+ax.set_ylim(.7, 1.001)
+ax.set_xlim(0, 365 * 5)
+ax.set_xticks([i * 365 for i in range(6)])
+ax.set_xticklabels([0, 1, 2, 3, 4, 5])
+ax.set_xlabel('Years', fontsize=12)
+ax.set_ylabel('Survival Probability', fontsize=12)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_position(['outward', 5])
+ax.spines['bottom'].set_position(['outward', 5])
+ax.spines['left'].set_bounds(.7, 1)
+ax.legend(frameon=False, title='TMB', ncol=5)
+plt.savefig(cwd / 'figures' / 'supp_fig1' / 'log_survival_curves.pdf')
+
+
+##single cutoff
+indexes = np.argsort(tmb)
+tmb = np.sort(tmb)
+
+
+times = samples['OS.time'].values[indexes]
+events = samples['OS'].values[indexes]
+stats = []
+offset = 25
+for index, cutoff in enumerate(tmb[offset: -offset]):
+    stats.append(logrank_test(times[: index + offset + 1], times[index + offset + 1:], event_observed_A=events[: index + offset + 1], event_observed_B=events[index + offset + 1:]).test_statistic)
+
+cutoff = np.argmax(stats)
+
+samples['high'] = samples['tmb'].apply(lambda x: (x >= tmb[cutoff + offset + 1]).astype(np.int16))
+
+def get_statistic(index):
+    stats = []
+    for index2 in range(len(tmb[offset + index: -(offset + offset2 - 1)])):
+        stats.append(logrank_test(np.concatenate([times[: index + offset + 1], times[index + offset + offset2 + index2 + 1:]]),
+                                  times[index + offset + 1: index + offset + offset2 + index2 + 1],
+                                  event_observed_A=np.concatenate([events[: index + offset + 1], events[index + offset + offset2 + index2 + 1:]]),
+                                  event_observed_B=events[index + offset + 1: index + offset + offset2 + index2 + 1]
+                                  ).test_statistic)
+    return stats
+
+stats = {}
+offset = 25
+offset2 = 50
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+    for index, result in tqdm(zip(range(len(tmb[offset: -(offset + offset2 - 1)])), executor.map(get_statistic, range(len(tmb[offset: -(offset + offset2 - 1)]))))):
+        stats[index] = result
+
+max_stat = 0
+for i in stats:
+    if max(stats[i]) > max_stat:
+        max_stat = max(stats[i])
+        max_index = i
+
+index = max_index
+index2 = np.argmax(stats[max_index])
+
+#index=15
+#index2=255
+
+samples['mid'] = samples['tmb'].apply(lambda x: ((x >= tmb[index + offset + 1]) and (x <= tmb[index + offset + offset2 + index2])).astype(np.int16))
+
+kmf = KaplanMeierFitter()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+kmf.fit(samples.loc[samples['high'] == False]['OS.time'].values, samples.loc[samples['high'] == False]['OS'].values, label='Low TMB').plot_survival_function(ax=ax, show_censors=True, ci_alpha=0, censor_styles={"marker": "|", "ms":5})
+kmf.fit(samples.loc[samples['high'] == True]['OS.time'].values, samples.loc[samples['high'] == True]['OS'].values, label='High TMB').plot_survival_function(ax=ax, show_censors=True, ci_alpha=0, censor_styles={"marker": "|", "ms":5})
+
+kmf.fit(samples.loc[samples['mid'] == False]['OS.time'].values, samples.loc[samples['mid'] == False]['OS'].values, label='Moderate TMB').plot_survival_function(ax=ax, show_censors=True, ci_alpha=0, censor_styles={"marker": "|", "ms":5})
+kmf.fit(samples.loc[samples['mid'] == True]['OS.time'].values, samples.loc[samples['mid'] == True]['OS'].values, label='Extreme TMB').plot_survival_function(ax=ax, show_censors=True, ci_alpha=0, censor_styles={"marker": "|", "ms":5})
+
+
+
